461096c3243293c3579e21ed630141574c03a827
Amuan <neoncoder77@gmail.com>
2023-10-22 23:05:20 +0630

    my very first basic react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
+import React from "react";
+import './style.css';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class Header extends React.Component{
+  render(){
+    return(
+      <h1 className="title">A Very Basic React App</h1>
+    )
+  }
+}
+class Body extends React.Component{
+  render(){
+    return(
+      <tr className="row">
+         
+         <td className="item">
+              {this.props.name}
+            </td>
+            <td className="item">
+              {this.props.qty}
+            </td>
+      </tr>
+    )
+  }
+}
+
+
+class App extends React.Component {
+  state = {
+    items : [
+      {id:1, name:'table', qty:5},
+      {id:2, name:'spoon', qty:3},
+   
+
+    ]
+  }
+ nameref = React.createRef();
+ qtyref = React.createRef();
+
+
+add=()=>{
+  let id = this.state.items.length+1;
+  let name = this.nameref.current.value;
+  let qty = this.qtyref.current.value;
+
+  console.log(id);
+  this.setState({
+    items : [
+      ...this.state.items,{id,name,qty}
+    ]
+  })
+
+}
+
+
+  render(){
+    return(
+      <div className="container">
+      <img src={logo} className="App-logo" alt="logo" />
+
+      <Header/>
+      <div>
+        <input className="in" type="text" ref={this.nameref}/><br/>
+        <input className="in" type="number" ref={this.qtyref}/><br/>
+
+      </div><br/>
+      <button className="btn" onClick={this.add}>Add new Item</button>
+      <table className="table">
+        <th>
+          <td>Item Lists</td>
+          
+        </th>
+        
+          {this.state.items.map(i=>{
+        return (
+          <Body key={i.id} name={i.name} qty={i.qty}/>
+
+        )
+          })}
+
+        
+      </table>
+
+      </div>
+    )
+  }
 }
 
 export default App;
